5c6107172b7a765a153c71094c0d683d1d84fd90
maczyt <zhouyutao@youzan.com>
2020-10-06 21:41:07 +0800

    add: 增加 map-editor

---
+++ Week 02/constant.js
@@ -0,0 +1,19 @@
+export function generatePatterns(num) {
+  return Array.from({ length: num * num }, () => 0)
+}
+
+export function delegate(container, target, eventName, callback) {
+  container.addEventListener(eventName, (event) => {
+    if (event.target.matches(target)) {
+      callback(event)
+    }
+  }, false)
+}
+export const color_val_symbol = Symbol('Color Value')
+export const color = {
+  [color_val_symbol]: 1,
+}
+
+export function clone(arr) {
+  return Object.create(arr)
+}
